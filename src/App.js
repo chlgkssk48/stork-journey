b95725f9469e325855e5f0b30adc0a0e91848a12
HannaChoi <hn4880@gmail.com>
2021-10-05 22:13:45 +0900

    Style: Create global style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+  }
+
+  body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
 export default function App() {
   return (
-    <></>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
